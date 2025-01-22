86ea34d0c667425c481268ec0a4eca8d3357b0c4
NatalieC2k <nataliec2k5@gmail.com>
2025-01-22 12:21:42 -0330

    big time cleanup

--- associate.py
+++ associate.py
@@ -1,50 +1,44 @@
 import sys
 import os
-from typing_extensions import override
-from openai import AssistantEventHandler, OpenAI
+from openai import OpenAI
 import json
 from lxml import etree
 import zipfile
 import shutil
-import re
 
 prompt = sys.argv[0]
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-input = "Change the name in all documents to “Amy Alpha” and adjust her salary to $100,000"
-
-assistant = client.beta.assistants.create(
-    name="Legal Assistant",
-    instructions="you are legal assistant think things through",
-    temperature=0,
-    model="gpt-4o",
-    tools=[
-        {"type": "file_search"},
-
+tools = [
         {
             "type": "function",
             "function": {
-                "name": "change_text",
-                "description": "change text in document",
+                "name": "replace_text",
+                "description": "replace text in document in all instances",
                 "parameters": {
                     "type": "object",
                     "properties": {
-                        "text_in_document": {
+                        "text_to_replace": {
                             "type": "string",
-                            "description": "text currently in document that should be changed"
+                            "description": "text currently in document that should be changed add word or two before",
                         },
                         "text_to_change_to": {
                             "type": "string",
-                            "description": "text text_in_document should be changed to"
-                        }
+                            "description": "text text_to_replace should be changed to",
+                        },
                     },
-                    "required": ["text_in_document", "text_to_change_to"],
+                    "required": ["text_to_replace", "text_to_change_to"],
+                    "additionalProperties": False,
                 },
+                "strict": True,
             },
         },
-    ],
+    ]
+
+input = (
+    "Change the name in all documents to “Amy Alpha” and adjust her salary to $100,000"
 )
 
 if os.path.isdir("temp"):
@@ -56,159 +50,84 @@ if os.path.isdir("output"):
 directory = "Employment Agreement"
 files = []
 for dirpath, dirnames, filenames in os.walk(directory):
-    noextensions = map(lambda filename : filename.split('.')[0], filenames)
+    noextensions = map(lambda filename: filename.split(".")[0], filenames)
     files.extend(noextensions)
 
+
 os.mkdir("temp")
-for filename in files:
-    unzipped_path = f"temp/{filename}"
-    with zipfile.ZipFile(f"{directory}/{filename}.docx", 'r') as zip_ref:
-        os.mkdir(unzipped_path)
-        zip_ref.extractall(unzipped_path)
-
-    tree = etree.parse(unzipped_path + '/word/document.xml')
-    notags = etree.tostring(tree,  method='text', encoding='UTF-8')
-    intermediary_file = open(f"{unzipped_path}.txt", "wb+")
-    intermediary_file.write(notags)
-    intermediary_file.close()
-
-
-def change_name(current_text: str, arguments):
-    name_to_change = arguments["name_to_change"]
-    name_to_change_to = arguments["name_to_change_to"]
-    print(f"\nchanging name \"{name_to_change}\" -> \"{name_to_change_to}\"")
-
-    i = 0
-    for name in name_to_change.split(' '):
-        pattern = re.compile(name, re.IGNORECASE)
-        current_text = pattern.sub(name_to_change_to.split(' ')[i], current_text)
-        i += 1
-    return current_text
-
-def change_text(current_text: str, arguments):
-    text_in_document = arguments["text_in_document"]
-    text_to_change_to = arguments["text_to_change_to"]
-    print(f"\nchanging text \"{text_in_document}\" -> \"{text_to_change_to}\"")
-
-    pattern = re.compile(text_in_document, re.IGNORECASE)
-    return pattern.sub(text_to_change_to, current_text)
-
 os.mkdir("output")
+
+
 def zip_directory(folder_path, output_zipfile):
-    with zipfile.ZipFile(output_zipfile, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(output_zipfile, "w", zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 file_path = os.path.join(root, file)
                 zipf.write(file_path, os.path.relpath(file_path, folder_path))
 
+
+def change_text(current_text: str, text_in_document, text_to_change_to):
+    print(f'\nchanging text "{text_in_document}" -> "{text_to_change_to}"')
+
+    return current_text.replace(text_in_document, text_to_change_to)
+
+
 for filename in files:
-    vector_store = client.beta.vector_stores.create(name=filename)
+    print("\n", filename)
+    unzipped_path = f"temp/{filename}"
+    with zipfile.ZipFile(f"{directory}/{filename}.docx", "r") as zip_ref:
+        os.mkdir(unzipped_path)
+        zip_ref.extractall(unzipped_path)
 
-    file_streams = [open(f"temp/{filename}.txt", "rb")]
-    file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-        vector_store_id=vector_store.id, files=file_streams
-    )
-    assistant = client.beta.assistants.update(
-        assistant_id=assistant.id,
-        tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
-    )
+    tree = etree.parse(unzipped_path + "/word/document.xml")
+    notags = etree.tostring(tree, method="text", encoding="UTF-8")
 
-    class EventHandler(AssistantEventHandler):
-        @override
-        def on_text_created(self, text) -> None:
-            print("\nassistant > ", end="", flush=True)
+    client = OpenAI()
 
-        @override
-        def on_tool_call_created(self, tool_call):
-            pass
-        @override
-        def on_event(self, event):
-            # Retrieve events that are denoted with 'requires_action'
-            # since these will have our tool_calls
-            if event.event == 'thread.run.requires_action':
-                run_id = event.data.id  # Retrieve the run ID from the event data
-                self.handle_requires_action(event.data, run_id)
-
-        def handle_requires_action(self, data, run_id):            
-            temp_docx_directory = f"temp/{filename}/word/document.xml"
-            temp_file = open(temp_docx_directory)
-            file_text = temp_file.read()
-            temp_file.close()
-
-            for tool in data.required_action.submit_tool_outputs.tool_calls:
-                arg_json = json.loads(tool.function.arguments)
-                if tool.function.name == "change_name":
-                    file_text = change_name(file_text, arg_json)
-                elif tool.function.name == "change_text":
-                    file_text = change_text(file_text, arg_json)
-
-            os.remove(temp_docx_directory)
-
-            output_file = open(temp_docx_directory, 'w+')
-            output_file.write(file_text)
-            output_file.close()
-
-        def submit_tool_outputs(self, tool_outputs, run_id):
-            # Use the submit_tool_outputs_stream helper
-           with client.beta.threads.runs.submit_tool_outputs_stream(
-            thread_id=self.current_run.thread_id,
-            run_id=self.current_run.id,
-            tool_outputs=tool_outputs,
-            event_handler=EventHandler(),
-            ) as stream:
-                for text in stream.text_deltas:
-                    print(text, end="", flush=True)
-                print()
-
-        @override
-        def on_message_done(self, message) -> None:
-            # print a citation to the file searched
-            message_content = message.content[0].text
-            annotations = message_content.annotations
-            citations = []
-            for index, annotation in enumerate(annotations):
-                message_content.value = message_content.value.replace(
-                    annotation.text, f"[{index}]"
-                )
-                if file_citation := getattr(annotation, "file_citation", None):
-                    cited_file = client.files.retrieve(file_citation.file_id)
-                    citations.append(f"[{index}] {cited_file.filename}")
-
-            print(message_content.value)
-            print("\n".join(citations))
-
-    message_file = client.files.create(
-    file=open(f"temp/{filename}.txt", "rb"), purpose="assistants"
-    )
-    thread = client.beta.threads.create(
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        temperature=0,
         messages=[
+            {"role": "developer", "content": "you are legal ai"},
             {
                 "role": "user",
                 "content": f"""
-    Instructions:
-    - {input}
-    Goals
-    - perserve meaning if possible
-    - use Instructions to alter document thouroughly think it through
-    - If a name is changed it must be change throughout first and last name
+                Input:
+                - {input}
+
+                Instructions:
+                - MAKE CHANGES TO Document Text BASED ON Input ABOVE AND Document Text BELOW
+                - DON'T FILL IN BLANKS UNLESS SPECIFICALLY ASKED
+                - MAINTAIN CASE
+                - MINIMAL CHANGES PERSERVE MEANING IF POSSIBLE
+
+                Document Text:
+                - {notags}
                 """,
-                "attachments": [
-                    {"file_id": message_file.id, "tools": [{"type": "file_search"}]}
-                ],
-            }
-        ]
+            },
+        ],
+        tools=tools,
     )
 
-    with client.beta.threads.runs.stream(
-        thread_id=thread.id,
-        assistant_id=assistant.id,
-        instructions=input,
-        event_handler=EventHandler(),
-    ) as stream:
-        stream.until_done()
-        print(f"\n{filename} is done!")
-        zip_directory(f"temp/{filename}", f"output/{filename}.docx")
 
+    temp_docx_directory = f"temp/{filename}/word/document.xml"
+    temp_file = open(temp_docx_directory)
+    file_text = temp_file.read()
+    temp_file.close()
 
+    for tool_call in completion.choices[0].message.tool_calls:
+        json_args = json.loads(tool_call.function.arguments)
+        print(json_args)
+        text_to_replace = json_args["text_to_replace"]
+        text_to_change_to = json_args["text_to_change_to"]
+        file_text = change_text(file_text, text_to_replace, text_to_change_to)  
+
+    os.remove(temp_docx_directory)
+
+    output_file = open(temp_docx_directory, "w+")
+    output_file.write(file_text)
+    output_file.close()
+
+    zip_directory(f"temp/{filename}", f"output/{filename}.docx")
 
 shutil.rmtree("temp")
---
+++ part2.py
@@ -0,0 +1,270 @@
+import sys
+import os
+from openai import OpenAI
+import json
+from lxml import etree
+import zipfile
+import shutil
+from functools import reduce
+
+
+prompt = sys.argv[0]
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "replace_text",
+            "description": "replace text in document in all instances",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "text_to_replace": {
+                        "type": "string",
+                        "description": "text in document to be replaced",
+                    },
+                    "text_to_insert": {
+                        "type": "string",
+                        "description": "text that will be inserted where text_to_replace i sfound",
+                    },
+                },
+                "required": ["text_to_replace", "text_to_insert"],
+                "additionalProperties": False,
+            },
+            "strict": True,
+        },
+    },
+]
+
+
+if os.path.isdir("temp"):
+    shutil.rmtree("temp")
+if os.path.isdir("output"):
+    shutil.rmtree("output")
+
+
+directory = "NVCA"
+files = []
+for dirpath, dirnames, filenames in os.walk(directory):
+    noextensions = map(lambda filename: filename.split(".")[0], filenames)
+    files.extend(noextensions)
+
+
+os.mkdir("temp")
+os.mkdir("output")
+
+
+def find_text_xml(text, text_to_find):
+    tag_freeze = False
+    found_index = 0
+    text_counter = 0
+    i = 0
+    while i < len(text):
+        match text[i]:
+            case "<":
+                tag_freeze = True
+            case ">":
+                tag_freeze = False
+            case _:
+                if not tag_freeze and text[i] == text_to_find[text_counter]:
+                    if text_counter == 0:
+                        found_index = i
+                    
+                    text_counter += 1
+
+                    if text_counter == len(text_to_find):
+                        return found_index
+                    
+                elif not tag_freeze and text_counter > 0:
+                    text_counter = 0
+        i += 1
+    return None
+
+
+def replace_text_xml(text, text_to_replace, text_to_insert):    
+    tag_freeze = False
+    insertion_counter = 0
+    non_frozen_counter = 0
+
+    i = find_text_xml(text, text_to_replace)
+    if i is None:
+        return text
+    
+    text = list(text)
+    while len(text_to_insert) > insertion_counter or len(text_to_replace) > non_frozen_counter:
+        if insertion_counter < len(text_to_insert) and len(text_to_replace) <= non_frozen_counter:
+            text.insert(i, text_to_insert[insertion_counter])
+            i += 1
+            insertion_counter += 1
+            non_frozen_counter += 1
+            continue
+
+        match text[i]:
+            case None:
+                return "".join(text)
+            case "<":
+                tag_freeze = True
+            case ">":
+                tag_freeze = False
+            case _:
+                if tag_freeze:
+                    break
+                else:
+                    if insertion_counter >= len(text_to_insert) and len(text_to_replace) > non_frozen_counter:
+                        text[i] = ""
+                    else:
+                        text[i] = text_to_insert[insertion_counter]
+                        insertion_counter += 1
+                non_frozen_counter += 1
+        i += 1
+
+    return "".join(text)
+
+def replace_text(text, text_to_replace, text_to_insert):
+        print(f'\Replacing text "{text_to_replace}" -> "{text_to_insert}"')
+        updated_text = replace_text_xml(text, text_to_replace, text_to_insert)
+        if updated_text == text:
+            print("Text not found no replacement")
+        else:
+            print("Text found and replaced")
+        return updated_text
+
+def zip_directory(folder_path, output_zipfile):
+    with zipfile.ZipFile(output_zipfile, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, folder_path))
+
+
+for filename in files:
+    unzipped_path = f"temp/{filename}"
+    with zipfile.ZipFile(f"{directory}/{filename}.docx", "r") as zip_ref:
+        os.mkdir(unzipped_path)
+        zip_ref.extractall(unzipped_path)
+
+tree = etree.parse("temp/acme-motors-term-sheet/word/document.xml")
+term_sheet = etree.tostring(tree, method="text", encoding="UTF-8").decode("utf-8")
+
+simplify_term_sheet = False
+if simplify_term_sheet:
+    completion = client.chat.completions.create(
+            model="gpt-4o",
+            temperature=0,
+            messages=[
+                {"role": "developer", "content": "The agent is doing legal work"},
+                {
+                    "role": "user",
+                    "content": f"""
+                        The agent will minimize the term sheet retaining names and figures given.
+
+                        Term Sheet: \"\"\"
+                        {term_sheet}
+                        \"\"\"
+                        """,
+                },
+            ],
+            tools=tools,
+        )
+    term_sheet = completion.choices[0].message.content
+
+for filename in files:
+    if filename == "acme-motors-term-sheet":
+        continue
+
+    print(filename)
+
+    tree = etree.parse(unzipped_path + "/word/document.xml")
+    notags = etree.tostring(tree, method="text", encoding="UTF-8").decode("utf-8")
+
+    split_notags = notags.split(" ")
+    n = len(split_notags) // 4
+
+    slices = [split_notags[i : i + n] for i in range(0, len(split_notags), n)]
+
+    if len(split_notags) % 4 != 0:
+        slices[-1].extend(split_notags[len(slices[0]) * 3 :])
+
+    def concat(a, b):
+        return a + " " + b
+
+    slices = list(map(lambda slice: reduce(concat, slice), slices))
+
+    temp_docx_directory = f"temp/{filename}/word/document.xml"
+    temp_file = open(temp_docx_directory)
+    file_text = temp_file.read()
+    temp_file.close()
+
+    i = 0
+    for slice in slices:
+        i += 1
+
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            temperature=0,
+            messages=[
+                {"role": "developer", "content": "The agent is doing legal work"},
+                {
+                    "role": "user",
+                    "content": f"""
+                    The agent will return only a single integer without explanation which is the number of blanks to be filled within Document Text.
+
+                    Document Text: \"\"\"
+                    {slice}
+                    \"\"\"
+                    """,
+                },
+            ],
+        )
+
+        number_of_blanks = int(completion.choices[0].message.content)
+        print("Number Of Blanks: ", number_of_blanks)
+        if number_of_blanks == 0:
+            continue
+
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            temperature=0,
+            messages=[
+                {"role": "developer", "content": "The agent is doing legal work"},
+                {
+                    "role": "user",
+                    "content": f"""
+                    The agent will fill in {number_of_blanks} blanks in Document Text using multiple tool calls if encountering atypical characters using information from Term Sheet.
+
+                    Term Sheet: \"\"\"
+                    {term_sheet}
+                    \"\"\"
+
+                    Document Text: \"\"\"
+                    {slice}
+                    \"\"\"
+                    """,
+                },
+            ],
+            tools=tools,
+        )
+
+        if completion.choices[0].message.tool_calls is None:
+            continue
+
+        for tool_call in completion.choices[0].message.tool_calls:
+            json_args = json.loads(tool_call.function.arguments)
+            
+            text_to_replace = json_args["text_to_replace"]
+            text_to_insert = json_args["text_to_insert"]
+
+            file_text = replace_text(file_text, text_to_replace, text_to_insert)
+
+    os.remove(temp_docx_directory)
+
+    output_file = open(temp_docx_directory, "w+")
+    output_file.write(file_text)
+    output_file.close()
+
+    zip_directory(f"temp/{filename}", f"output/{filename}.docx")
+
+# shutil.rmtree("temp")
